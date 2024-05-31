561cfeed4fccf7aa64edc9c9a71b093240a7e8a3
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-05-31 23:53:31 +0530

    Navbar modified and wallpapers page added

---
+++ app/wallpapers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WallpapersPage = () => {
+  return <div className="min-h-screen">WallpapersPage</div>;
+};
+
+export default WallpapersPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,48 +16,27 @@ const Navbar: React.FC = ({ className }: { className?: string }) => {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
+        <MenuItem setActive={setActive} active={active} item="Company">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/about">About</HoveredLink>
+            <HoveredLink href="/history">History</HoveredLink>
+            <HoveredLink href="/join">Join the team</HoveredLink>
+            <HoveredLink href="/blog">Blog</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-            <ProductItem
-              title="Algochurn"
-              href="https://algochurn.com"
-              src="https://assets.aceternity.com/demos/algochurn.webp"
-              description="Prepare for tech interviews like never before."
-            />
-            <ProductItem
-              title="Tailwind Master Kit"
-              href="https://tailwindmasterkit.com"
-              src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-              description="Production ready Tailwind css components for your next project"
-            />
-            <ProductItem
-              title="Moonbeam"
-              href="https://gomoonbeam.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-              description="Never write from scratch again. Go from idea to blog in minutes."
-            />
-            <ProductItem
-              title="Rogue"
-              href="https://userogue.com"
-              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-            />
+        <MenuItem setActive={setActive} active={active} item="Product">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/developers-api">Developers API</HoveredLink>
+            <HoveredLink href="/dataset">Dataset</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
+        <MenuItem setActive={setActive} active={active} item="Community">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            <HoveredLink href="/contributor">Become a contributor</HoveredLink>
+            <HoveredLink href="/topics">Topics</HoveredLink>
+            <HoveredLink href="/collections">Collections</HoveredLink>
+            <HoveredLink href="/trends">Trends</HoveredLink>
+            <HoveredLink href="/awards">Awards</HoveredLink>
           </div>
         </MenuItem>
       </Menu>
